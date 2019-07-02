73fde42f659b6421268dbd2ffb73414a5fb77c1f
gwcadmin <gwcadmin@gwcadmins-MacBook-Pro.local>
2019-07-02 13:12:10 -0400

    Finish filtergram and part 1 of exercise

--- unit1/u1.8/filtergram.py
+++ unit1/u1.8/filtergram.py
@@ -1,7 +1,15 @@
-from filters.py import *
+from filters import *
 
 """
 Part 1:
 - Load image from computer
 - Save image to new file on computer under different filename
 """
+
+filename = 'chikorita.png'
+savename = 'chikorita2.png'
+
+if __name__ == "__main__":
+    im = load_img(filename)
+    show_img(im)
+    save_img(im, savename)
