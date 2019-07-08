f94ad49bf2388c66ee53903038f4b84f934c2ce6
gwcadmin <gwcadmin@gwcadmins-MacBook-Pro.local>
2019-07-08 14:30:06 -0400

    Finish part 2

--- unit2/u2.2/data_vis_project_starter.py
+++ unit2/u2.2/data_vis_project_starter.py
@@ -7,6 +7,7 @@ need!
 import json
 from textblob import TextBlob
 import matplotlib.pyplot as plt
+import numpy as np
 
 def getAveragePolaritySubjectivity(sentiments):
     totalPolarity = 0
@@ -16,7 +17,44 @@ def getAveragePolaritySubjectivity(sentiments):
         totalSubjectivity += sentiment.subjectivity
     return (totalPolarity/len(sentiments), totalSubjectivity/len(sentiments))
 
+def getPolarityArray(sentiments):
+    polarities = []
+    for sentiment in sentiments:
+        polarities.append(sentiment.polarity)
+    return polarities
+
+def getSubjectivityArray(sentiments):
+    subjectivities = []
+    for sentiment in sentiments:
+        subjectivities.append(sentiment.subjectivity)
+    return subjectivities
+
+def graphHistogram(data, minX, maxX, xLabel, yLabel, title, figure_num):
+    plt.figure(figure_num)
+    num_bins = 20
+    n, bins, patches = plt.hist(data, num_bins, facecolor='g', alpha=0.75)
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+    plt.title(title)
+    plt.axis([minX, maxX, 0, len(data)]) # min and max for x axis, then y axis
+    plt.grid(True)
+    plt.show()
+
+def graphScatterplot(sentiments):
+    plt.figure(1)
+    np.random.seed(19680801)
+
+    N = len(sentiments)
+    x = getPolarityArray(sentiments)
+    y = getSubjectivityArray(sentiments)
+    colors = np.random.rand(N)
+    area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
+
+    plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+    plt.show()
+# array containing all Sentiment objects
 allSentiments = []
+
 #Get the JSON data
 tweetFile = open("../u2.1/tweets_small.json", "r")
 tweetData = json.load(tweetFile)
@@ -30,6 +68,17 @@ for tweet in tweetData:
     allSentiments.append(blob.sentiment)
 tweetFile.close()
 
+# print average polarities and subjectivities
 averages = getAveragePolaritySubjectivity(allSentiments)
 print(f'Average polarity: {averages[0]}')
 print(f'Average subjectivity: {averages[1]}')
+print('')
+
+graphScatterplot(allSentiments)
+# graph polarity histogram
+polarities = getPolarityArray(allSentiments)
+graphHistogram(polarities, -1, 1, 'Polarity Values', 'Number of tweets', 'Histogram of Polarity', 2)
+
+# graph subjectivity histogram
+subjectivities = getSubjectivityArray(allSentiments)
+graphHistogram(subjectivities, 0, 1, 'Subjectivity Values', 'Number of tweets', 'Histogram of Subjectivity', 3)
