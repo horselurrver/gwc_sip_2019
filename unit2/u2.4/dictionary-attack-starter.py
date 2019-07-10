611f87aef82c2dee1ac24e97bf020dfe57c417ff
gwcadmin <gwcadmin@gwcadmins-MacBook-Pro.local>
2019-07-10 11:15:20 -0400

    Finish dictionary attack exercise. Go difflib

--- unit2/u2.4/dictionary-attack-starter.py
+++ unit2/u2.4/dictionary-attack-starter.py
@@ -1,10 +1,29 @@
 """
 Useful url: https://stackoverflow.com/questions/10018679/python-find-closest-string-from-a-list-to-another-string
 """
+import difflib
 
+NUM_MATCHES = 10
+# returns new word with non alphabetic letters removed
+def removeNonAlpha(word):
+    result = ''
+    for char in word:
+        if char.isalpha():
+            result = result + char
+    return result
 # returns whether or not word is in dictionary
 def wordInDict(word, dict):
     return word in dict
+
+# returns whether word is a close match to a dictionary word
+def findCloseMatches(word, dict):
+    closeMatches = difflib.get_close_matches(word, dict, NUM_MATCHES)
+    # removes the word itself from the close matches
+    if word in closeMatches:
+        closeMatches.remove(word)
+
+    return closeMatches
+
 # returns whether or not the word contains dictionary words
 def containsDictWords(word, dict):
     wordsContained = []
@@ -19,7 +38,7 @@ with open("dictionary.txt","r") as f:
     dictionary = f.readlines()
 
 dictionary = [x.strip() for x in dictionary if len(x.strip()) > 1]
-print(str(dictionary))
+# print(str(dictionary))
 
 # ask user for password input
 test_password = input("Type in a trial password: ")
@@ -34,4 +53,8 @@ else:
         print('Wow! Your password contains 0 dictionary words!')
     else:
         words = ', '.join(wordsContained)
+        # create variable for password without non alphabetic characters
+        onlyAlphabet = removeNonAlpha(test_password)
         print(f'Sad face... your password contains the following words: {words}')
+        closeMatches = findCloseMatches(onlyAlphabet, dictionary)
+        print(f'Top {NUM_MATCHES} close matches: ' + ', '.join(closeMatches)) if len(closeMatches) > 0 else print('No close matches found')
